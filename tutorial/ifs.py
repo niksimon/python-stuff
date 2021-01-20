254eb7fff8b02d1376034ac0edf550f76df4aea1
simon <nikolasimonovic94@gmail.com>
2021-01-20 01:55:47 +0100

    :)

---
+++ tutorial/ifs.py
@@ -0,0 +1,9 @@
+is_male = False
+is_tall = True
+
+if is_male and not is_tall:
+    print("hi")
+elif is_male and is_tall:
+    print("hi 2")
+else:
+    print("hey")
---
+++ tutorial/return.py
@@ -0,0 +1,6 @@
+def cube(x):
+    return x ** 3
+
+c = cube(3)
+
+print(c)
